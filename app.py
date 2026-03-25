10527854a37777239884327be280fe7f451d2da7
kenshin <wj980917151@gmail.com>
2026-03-25 14:13:46 +0900

    fix deploy with fetch reset

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "This a version2."
+    return "This a version4."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
